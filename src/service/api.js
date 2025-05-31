50321944866d35bce970baf84ec654779b021fc0
Saulo Morais <107441433+LitoDev25@users.noreply.github.com>
2025-05-30 23:35:03 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://filmewatch-1.onrender.com/",
+    baseURL: "https://watchanim.litodev.site/",
 })
 
 export default api;
